476d2ec86d60487e558bc8fcb98074ea1c325bf5
SamaraM-Anjos <132007421+SamaraM-Anjos@users.noreply.github.com>
2024-04-05 17:37:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// const christine={};
+// christine.nome = "Christine";
+// christine.saudar = () => "Xablau";
+// christine.endereco = {logradoura: "Rua Tiradentes ",
+//                       cidade: "Nova Friburgo", bairro: "Centro"};
+// christine["data_de_nascimento"] = "14/07/2007"; //ou christine["data de nascimento"] (apenas se for feita a escrita como se fosse de um vetor)
+// console.log(christine.nome);
+// console.log(christine.saudar);
+// console.log(christine.endereco.bairro);
+
+
+// const btn = document.querySelector("#executar");
+// btn.addEventListener("click", () => {
+//     const enderecoJean = {
+//         bairro:"",
+//         logradouro: "",
+//         estado_info: {
+//             area_km: "",
+//             codigo_ibge:"33",
+//             nome: "RJ",
+//         },
+//         cep: "",
+
+//     };
+// });
+
+//Retornando objeto literal
+// const aluno = function (nome, dataNascimento, sexo){
+//     return {
+//         nome:  nome,
+//         data_do_nascimento: dataNascimento,
+//         sexo: sexo
+//     }
+// }
+
+// ana = aluno("Ana", "24/07/2006", "F");
+//Função construtora
+// const aluno = function (nome, dataNascimento, sexo){
+//         this.nome:  nome;
+//         this.data_do_nascimento: dataNascimento;
+//         this.sexo: sexo;
+//     }
+// }
+// ana = new aluno("Ana", "24/07/2006", "F");
+
+//delete pessoa.telefone;
+
+// const pedro = ("Pedro", "11/11/2011", "M");
+// delete pedro.dataNascimento;
+// console.log(pedro);
+
+
+// --------Array---------
+
+const array = [];
+array.push(1234);//.push() - adiciona no final do array
+array.unshift("yay");//.unshift() - ele adiciona o item/dado no  início do array
+array.pop(); //.pop() remove o último da posição do array
+array.shift; //.shift() rmeove o elemento da primeira posição
+// . length, .sort, .join(), .split() - para string
+
